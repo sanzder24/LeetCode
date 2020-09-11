68376157d691b9ca7f2ce912a62cbb8a33c6f2fc
msyedniz <msyedniz@uncc.edu>
2020-09-10 20:52:56 -0400

    10-SEP-2020

---
+++ Validate Binary Search Tree.java
@@ -0,0 +1,15 @@
+class Solution {
+     public boolean isValidBST(TreeNode root) {
+            return valid(root, null, null);
+        }
+
+        boolean valid(TreeNode root, Integer min, Integer max) {
+            if (root == null) {
+                return true;
+            }
+            if ((min != null && root.val <= min) || (max != null && root.val >= max)) {
+                return false;
+            }
+            return valid(root.left, min, root.val) && valid(root.right, root.val, max);
+        }
+}
