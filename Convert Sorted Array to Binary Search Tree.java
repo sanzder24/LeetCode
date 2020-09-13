c929896bc42a79d5ea8d72ed400610827e32edfe
msyedniz <msyedniz@uncc.edu>
2020-09-12 20:20:16 -0400

    12-SEP-2020

---
+++ Convert Sorted Array to Binary Search Tree.java
@@ -0,0 +1,16 @@
+class Solution {
+    public TreeNode sortedArrayToBST(int[] num) {
+            return dfs(num, 0, num.length - 1);
+        }
+
+        public TreeNode dfs(int[] num, int start, int end) {
+            if (start > end) {
+                return null;
+            }
+            int mid = (end + start) / 2;
+            TreeNode root = new TreeNode(num[mid]);
+            root.left = dfs(num, start, mid - 1);
+            root.right = dfs(num, mid + 1, end);
+            return root;
+        }
+}
