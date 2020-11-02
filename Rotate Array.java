ff054e3409a6ab621a2c9b545e6197bef5a6d996
msyedniz <msyedniz@uncc.edu>
2020-11-02 15:46:35 -0500

    some leetcode nov 2020

---
+++ Rotate Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+            int len = nums.length;
+            k %= len;
+            reverse(nums, 0, len - 1);
+            reverse(nums, 0, k - 1);
+            reverse(nums, k, len - 1);
+        }
+
+        private void reverse(int[] nums, int start, int end) {
+            while (start < end) {
+                int tmp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = tmp;
+                start++;
+                end--;
+            }
+        }
+}
