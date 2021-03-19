073585f80b87b09d708ac1e0c16fb335a5544023
msyedniz <msyedniz@uncc.edu>
2021-03-18 22:13:52 -0400

    18-03-2021

---
+++ Container with Most Water.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxArea(int[] height) {
+        
+            int max = 0;
+            int i = 0;
+            int j = height.length - 1;
+            while (i < j) {
+                max = Math.max(Math.min(height[i], height[j]) * (j - i), max);
+                if (height[i] <= height[j]) {
+                    i++;
+                } else {
+                    j--;
+                }
+            }
+            return max;
+        }
+}
