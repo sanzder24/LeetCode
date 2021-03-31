06f90c2f614e5f2cbd3a1b959f6984523325319d
msyedniz <msyedniz@uncc.edu>
2021-03-31 18:20:07 -0400

    Added

---
+++ Fruit into Baskets.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int totalFruit(int[] tree) {
+        if( tree ==null || tree.length ==0){
+            return 0;
+        }
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int j=0, i=0;
+        
+        int max =1;
+        while( j<tree.length ) {
+            if(map.size() <=2) {
+                map.put(tree[j], j++);
+            }
+            if(map.size()>2) {
+                int min =tree.length-1;
+                for( int value: map.values()) {     
+                min = Math.min(min, value);
+                }
+            i = min+1;
+            map.remove(tree[min]);
+            }
+            max = Math.max(max, j-i);
+        }
+        return max;
+    }
+}
