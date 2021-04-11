65a233cdecdb95930ddff7e4021d9c64cb94da5c
msyedniz <msyedniz@uncc.edu>
2021-04-11 18:40:05 -0400

    Apr-11-2021

---
+++ Longest word in a dictionary through deleting.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String findLongestWord(String s, List<String> dictionary) {
+        
+     Collections.sort(dictionary, (a, b) -> a.length() == b.length() ? a.compareTo(b) : b.length() -     a.length());
+            for (String dictWord : dictionary) {
+                int i = 0;
+                for (char c : s.toCharArray()) {
+                    if (i < dictWord.length() && dictWord.charAt(i) == c) {
+                        i++;
+                    }
+                }
+                if (i == dictWord.length()) {
+                    return dictWord;
+                }
+            }
+            return "";   
+    }
+}
