57fb6d8cf7dc03d6a6fdabc973c18bf4d4ecf0c3
msyedniz <msyedniz@uncc.edu>
2021-05-05 22:21:02 -0400

    May 5 21

--- Coin Change.java
+++ Coin Change.java
@@ -6,9 +6,9 @@ class Solution {
         for(int i=0; i<=amount;i++){
             for(int j=0;j<coins.length;j++){
                 if(coins[j]<=i){
-                     dp[i] = Math.min(dp[i], 1 + dp[i-coins[j]]);
-                }
-               
+                     dp[i] = Math.min(dp[i], 1 + dp[i-coins[j]]); //if taking the coin and checking prev value subtracting the coin
+                }  //example: 11 -> 5 + 5 + 1 // Dp[2] = min(2,1) from  1 (c) x 2 or 2 (c) x 1 
+                
             }
         }
         return dp[amount]>amount ? -1: dp[amount];
