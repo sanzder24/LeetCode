726a8de5cf04c0aee1e733185506a1d385f785d2
msyedniz <msyedniz@uncc.edu>
2021-05-10 17:03:13 -0400

    May-10-2021

--- Maximum Product Subarray.java
+++ Maximum Product Subarray.java
@@ -1,14 +1,14 @@
 class Solution {
     public int maxProduct(int[] nums) {
-            int pos = nums[0];
-            int neg = nums[0];
-            int max = nums[0];
+           int min = nums[0];
+           int max = nums[0];
+            int result =nums[0];
             for (int i = 1; i < nums.length; i++) {
-                int temp = pos;
-                pos = Math.max(nums[i], nums[i] * ((nums[i] >= 0) ? pos : neg));
-                neg = Math.min(nums[i], nums[i] * ((nums[i] >= 0) ? neg : temp));
-                max = Math.max(max, pos);
+               int temp_max = Math.max(nums[i] * max, Math.max(nums[i] * min, nums[i])); //track maximum subarray
+                        min = Math.min(nums[i] * max, Math.min(nums[i] * min, nums[i])); //track minimum subarray
+                max = temp_max;
+                result = Math.max(result, max);
             }
-            return max;
+            return result;
         }
-}
+} 
