11c754f3f5d9ac55b06d0d8da37b647bbb0cc93b
msyedniz <msyedniz@uncc.edu>
2021-05-12 13:43:06 -0400

    MAy 12 21

---
+++ Robot bounded in circle.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isRobotBounded(String instructions) {
+        int direction[][] = {{0,1}, {-1,0}, {0, -1}, {1 ,0}}; //up, left, down, right
+        int i =0, x=0, y=0;
+        for(int j=0; j<instructions.length(); j++){
+            if(instructions.charAt(j)=='L'){
+            i= (i+1)%4; //since 4 is the size and out of bound check
+            } else if (instructions.charAt(j)=='R'){
+            i= (i+3)%4;
+            } else {
+                x = x + direction[i][0];
+                y = y + direction[i][1];
+            }
+        }
+        return x==0 && y==0 || i!=0; //if rotation exist such as GL where 4 repetition is needed so chck i
+        
+    }
+}
