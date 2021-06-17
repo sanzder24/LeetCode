3f722f3449a1b2a5120168554c7e6b9276b6d353
msyedniz <msyedniz@uncc.edu>
2021-06-17 18:04:50 -0400

    June 17 21

--- Jump Game.java
+++ Jump Game.java
@@ -1,19 +1,11 @@
-public class Solution {
-    public boolean canJumpFromPosition(int position, int[] nums) {
-        if (position == nums.length - 1) {
-            return true;
-        }
-
-        int furtherjump=Math.min(position + nums[position],nums.length-1);
-        for( int nextposition=furtherjump; nextposition>position;nextposition--){
-            if(canJumpFromPosition(nextposition, nums)){
-                return true;
-            }
-        }
-        return false;
-    }
-
+class Solution {
     public boolean canJump(int[] nums) {
-        return canJumpFromPosition(0, nums);
+        int lastPos = nums.length -1;
+        for(int i =nums.length-1; i>=0; i--){
+        if((i + nums[i]) >= lastPos){
+            lastPos =i;
+         }
+        }
+        return lastPos == 0;
     }
 }
