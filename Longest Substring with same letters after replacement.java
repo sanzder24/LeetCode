80ec7300270920e65d0a5457a4cd5ef25d293da8
msyedniz <msyedniz@uncc.edu>
2021-07-07 09:08:32 -0400

    July 7th 2021

---
+++ Longest Substring with same letters after replacement.java
@@ -0,0 +1,28 @@
+class CharacterReplacement {
+  public static int findLength(String str, int k) {
+    int windowStart = 0, maxLength = 0, maxRepeatLetterCount = 0;
+    Map<Character, Integer> letterFrequencyMap = new HashMap<>();
+    // try to extend the range [ws, windowEnd]
+    for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+      char rightChar = str.charAt(windowEnd);
+      letterFrequencyMap.put(rightChar, letterFrequencyMap.getOrDefault(rightChar, 0) + 1);
+      maxRepeatLetterCount = Math.max(maxRepeatLetterCount, letterFrequencyMap.get(rightChar));
+
+      if (windowEnd - ws + 1 - maxRepeatLetterCount > k) { //substracting repeating characters to find max window
+        char leftChar = str.charAt(ws);
+        letterFrequencyMap.put(leftChar, letterFrequencyMap.get(leftChar) - 1);
+        ws++;
+      }
+
+      maxLength = Math.max(maxLength, windowEnd - ws + 1);
+    }
+
+    return maxLength;ws
+  }
+
+  public static void main(String[] args) {
+    System.out.println(CharacterReplacement.findLength("aabccbb", 2));
+    System.out.println(CharacterReplacement.findLength("abbcb", 1));
+    System.out.println(CharacterReplacement.findLength("abccde", 1));
+  }
+}
