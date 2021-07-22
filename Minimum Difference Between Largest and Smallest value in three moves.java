ecb753f7585eec7d9a267accbe1e6e019d0aa223
msyedniz <msyedniz@uncc.edu>
2021-07-22 18:08:47 -0400

    July 22

--- Minimum Difference Between Largest and Smallest value in three moves.java
+++ Minimum Difference Between Largest and Smallest value in three moves.java
@@ -1,22 +1,26 @@
-class Solution {
-    public int minDifference(int[] nums) {
-        if(nums.length <= 4) return 0;
-        PriorityQueue<Integer> maxQueue = new PriorityQueue<>();
-        PriorityQueue<Integer> minQueue = new PriorityQueue<>(Collections.reverseOrder());
-        for(int n : nums) {
-            maxQueue.add(n);
-            if(maxQueue.size() > 4) maxQueue.poll();
-            minQueue.add(n);
-            if(minQueue.size() > 4) minQueue.poll();
+    public int minDifference(int[] A) {
+        int n = A.length, res = Integer.MAX_VALUE;
+        if (n < 5) return 0;
+        Arrays.sort(A);
+        for (int i = 0; i < 4; ++i) {
+            res = Math.min(res, A[n - 4 + i] - A[i]);
         }
-        List<Integer> maxList = new ArrayList<>();
-        while(maxQueue.size() > 0) maxList.add(maxQueue.poll());
-        List<Integer> minList = new ArrayList<>();
-        while(minQueue.size() > 0) minList.add(minQueue.poll());
-        int ans = Integer.MAX_VALUE;
-        for(int i = 0; i <= 3; i++) {
-            ans = Math.min(ans, maxList.get(i) - minList.get(3-i));
-        }
-        return ans;
+        return res;
     }
-}
+
+    All three biggest elements can be replaced with 14
+[1,5,6,13,14,15,16,17] -> [1,5,6,13,14,14,14,14] == can be written as A[n-4] - A[0] == (14-1 = 13)
+
+Case 2: kill 2 biggest elements + 1 smallest elements
+
+[1,5,6,13,14,15,16,17] -> [5,5,6,13,14,15,15,15] == can be written as A[n-3] - A[1] == (15-5 = 10)
+
+Case 3: kill 1 biggest elements + 2 smallest elements
+
+[1,5,6,13,14,15,16,17] -> [6,6,6,13,14,15,16,16] == can be written as A[n-2] - A[2] == (16-6 = 10)
+
+Case 4: kill 3 smallest elements
+
+[1,5,6,13,14,15,16,17] -> [13,13,13,13,14,15,16,17] == can be written as A[n-1] - A[3] == (17-13 = 4)
+
+Answer is minimum of all these cases!
