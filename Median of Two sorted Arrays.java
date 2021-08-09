e059ede08b5eadfc116510bfbb3872e761bb545b
msyedniz <msyedniz@uncc.edu>
2021-08-09 10:01:50 -0400

    Aug 9

---
+++ Median of Two sorted Arrays.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+ int mindex = 0, nindex = 0, prevVal = 0, val = 0;
+
+        for (int i = 0; i <= (nums1.length + nums2.length)/2 ;i++) {
+            prevVal = val; //prevVal needed if merged array size is even
+            if(mindex == nums1.length) // Either empty array or all elements are smaller than other array
+                val = nums2[nindex++];
+            else if (nindex == nums2.length) // Either empty array or all elements are smaller than other array
+                val = nums1[mindex++];
+            else if (nums1[mindex] <= nums2[nindex]) 
+                val = nums1[mindex++];
+            else
+                val = nums2[nindex++];
+        }
+        
+        if ((nums1.length + nums2.length)%2 == 1) 
+            return val/1.0;
+        return (val + prevVal) / 2.0; 
+    }
+}
