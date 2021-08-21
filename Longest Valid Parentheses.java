da60de8b716932a545ecc7ff085af4e73d451607
msyedniz <msyedniz@uncc.edu>
2021-08-21 12:19:42 -0400

    Aug 21

--- Longest Valid Parentheses.java
+++ Longest Valid Parentheses.java
@@ -15,3 +15,24 @@ public class Solution {
         return maxans;
     }
 }
+class Solution2 {
+    public int longestValidParentheses(String s) {
+        int maxlen = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxlen = Math.max(maxlen, i - stack.peek());
+                }
+            }
+        }
+        return maxlen;
+    }
+}
